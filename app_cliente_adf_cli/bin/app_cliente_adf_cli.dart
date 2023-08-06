b84db2475fd023ba6396de6b47b74ed7ea97fd41
Paulo Weslley <vorfast@gmail.com>
2023-08-06 18:23:58 -0300

    inicio da configuração

---
+++ app_cliente_adf_cli/bin/app_cliente_adf_cli.dart
@@ -0,0 +1,5 @@
+import 'package:app_cliente_adf_cli/app_cliente_adf_cli.dart' as app_cliente_adf_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app_cliente_adf_cli.calculate()}!');
+}
---
+++ app_cliente_adf_cli/test/app_cliente_adf_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_cliente_adf_cli/app_cliente_adf_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
