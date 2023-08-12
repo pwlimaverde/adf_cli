60c51a04a456bff8764af009660fa9eefcd417cc
Paulo Weslley <vorfast@gmail.com>
2023-08-12 09:23:40 -0300

    inicio do desenvolvimento

--- app_cliente_adf_cli/bin/app_cliente_adf_cli.dart
+++ app_cliente_adf_cli/bin/app_cliente_adf_cli.dart
@@ -1,5 +1,4 @@
-import 'package:app_cliente_adf_cli/app_cliente_adf_cli.dart' as app_cliente_adf_cli;
 
 void main(List<String> arguments) {
-  print('Hello world: ${app_cliente_adf_cli.calculate()}!');
+
 }
