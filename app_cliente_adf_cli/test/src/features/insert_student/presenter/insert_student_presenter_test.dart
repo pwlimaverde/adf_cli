2ac03cda5553beff0cae39bff3b765ebf3f7f7d8
Paulo Weslley <vorfast@gmail.com>
2023-08-20 20:52:48 -0300

    delete ok

--- app_cliente_adf_cli/test/src/features/insert_student/presenter/insert_student_presenter_test.dart
+++ app_cliente_adf_cli/test/src/features/insert_student/presenter/insert_student_presenter_test.dart
@@ -4,7 +4,7 @@ import 'package:app_cliente_adf_cli/src/features/insert_student/presenter/insert
 import 'package:return_success_or_error/return_success_or_error.dart';
 import 'package:test/test.dart';
 
-final String json = jsonEncode({"name":"Gleidiane Limaverde","age":37,"nameCourses":["Academia Flutter","Jornada GetX"],"courses":[{"id":1,"name":"Academia do Flutter","isStudent":true},{"id":2,"name":"Jornada GetX","isStudent":true},{"id":3,"name":"Jornada Clean Code","isStudent":false},{"id":4,"name":"Jornada Arquitetura","isStudent":false}],"address":{"street":"Rua A","number":175,"zipCode":"0915225222","city":{"id":1,"name":"Santo André"},"phone":{"ddd":11,"phone":"985755555"}}});
+final String json = jsonEncode({"name":"Leticia Limaverde","age":37,"nameCourses":["Academia Flutter","Jornada GetX"],"courses":[{"id":1,"name":"Academia do Flutter","isStudent":true},{"id":2,"name":"Jornada GetX","isStudent":true},{"id":3,"name":"Jornada Clean Code","isStudent":false},{"id":4,"name":"Jornada Arquitetura","isStudent":false}],"address":{"street":"Rua A","number":175,"zipCode":"0915225222","city":{"id":1,"name":"Santo André"},"phone":{"ddd":11,"phone":"985755555"}}});
 
 void main() {
   test('carregar student find by id presenter deve retornar Sucess void',
